b8f5678d87e2e8c9864aa724e4210aaeafc10fb0
Muxlisa2007 <muxlisa200707@gmail.com>
2023-07-22 23:15:02 +0500

    4-masala

---
+++ j4.py
@@ -0,0 +1,32 @@
+
+a = int(input("Oy raqamini kiriting - > "))
+s=""
+match a:
+    case 1:
+        s="31"
+    case 2:
+        s="28"
+    case 3:
+        s="31"
+    case 4:
+        s="30"
+    case 5:
+        s="31"
+    case 6:
+        s="30"
+    case 7:
+        s="31"
+    case 8:
+        s="31"
+    case 9:
+        s="30"
+    case 10:
+        s="31"
+    case 11:
+        s="30"
+    case 12:
+        s="31"
+    case _:
+        
+        s="raqam notug`ri kiritildi"
+print(s)
