d30146b3d7985ae39a1ad66e7b39b786ce137d87
Muxlisa2007 <muxlisa200707@gmail.com>
2023-07-22 23:13:28 +0500

    3-masala

---
+++ j3.py
@@ -0,0 +1,32 @@
+
+a = int(input("Oy raqamini kiriting - > "))
+s=""
+match a:
+    case 1:
+        s="Qish"
+    case 2:
+        s="Qish"
+    case 3:
+        s="Bahor"
+    case 4:
+        s="Bahor"
+    case 5:
+        s="Bahor"
+    case 6:
+        s="Yoz"
+    case 7:
+        s="Yoz"
+    case 8:
+        s="Yoz"
+    case 9:
+        s="Kuz"
+    case 10:
+        s="Kuz"
+    case 11:
+        s="Kuz"
+    case 12:
+        s="Qish"
+    case _:
+        
+        s="raqam notug`ri kiritildi"
+print(s)
